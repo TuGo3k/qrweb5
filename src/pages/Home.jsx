e181e59b1c033280b8765d02b468d6ab5b2ddc15
TuGo3k <ATuru0106@gmail.com>
2024-12-22 23:38:57 +0800

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,7 +75,62 @@ export const Home = () => {
     { name: "Milagros", role: "Founder, Beauty Spa", image: "./ex2.jpg" },
     { name: "Agustina", role: "Founder, Beauty Spa", image: "./ex3.jpg" },
   ];
+  const priceplan = [
+    {
+      title: "BASIC PLAN",
+      price: 29,
+      image: "/pic1.jpg",
+      services: [
+        "✔ Phone & Email Support",
+        "✘ 3 Social Account",
+        "Branded Reports",
+        "Unlock rewards",
+        "Support Forum",
+      ],
+    },
+    {
+      title: "PRO PLAN",
+      price: 49,
+      image: "/pic2.jpg",
+      services: [
+        "✔ Phone & Email Support",
+        "✔ 3 Social Account",
+        "✔ Branded Reports",
+        "✔ Unlock rewards",
+        "✔ Support Forum",
+      ],
+    },
+    {
+      title: "PREMIUM PLAN",
+      price: 99,
+      image: "/pic3.jpg",
+      services: [
+        "✔ Phone & Email Support",
+        "✔ 3 Social Account",
+        "✔ Branded Reports",
+        "✔ Unlock rewards",
+        "✔ Support Forum",
+      ],
+    },
+  ];
 
+  const productCat = [
+    {
+      title: "Massage Soup",
+      img: "/img1.jpg",
+      price: 199.99,
+    },
+    {
+      title: "Massage Soup",
+      img: "/img2.jpg",
+      price: 159.99,
+    },
+    {
+      title: "Massage Soup",
+      img: "/img3.jpg",
+      price: 109.99,
+    },
+  ];
   const [chosen, setChosen] = useState(0);
 
   const selected =
@@ -102,15 +157,14 @@ export const Home = () => {
 
   const selectedData = getSelectedData(); // Get the selected option's data
   const [bg, setBg] = useState(0);
-  const Gallery =[
-    
-    {title: "HAIR CUT", image: './pic2.jpg'},
-    {title: "FOOT", image: './pic3.jpg'},
-    {title: "BODY", image: './pic4.jpg'},
-    {title: "MASSAGE", image: './pic5.jpg'},
-    {title: "FACE MASSAGE", image: './pic6.jpg'},
-]
- const [activeCategory, setActiveCategory] = useState("All");
+  const Gallery = [
+    { title: "HAIR CUT", image: "./pic2.jpg" },
+    { title: "FOOT", image: "./pic3.jpg" },
+    { title: "BODY", image: "./pic4.jpg" },
+    { title: "MASSAGE", image: "./pic5.jpg" },
+    { title: "FACE MASSAGE", image: "./pic6.jpg" },
+  ];
+  const [activeCategory, setActiveCategory] = useState("All");
 
   // Combine all pictures for the "All" category
   // const allPics = Gallery.flatMap((category) => category.image);
@@ -121,12 +175,12 @@ export const Home = () => {
   //     ? allPics
   //     : Gallery.find((category) => category.title === activeCategory)?.image ||
   //       [];
-   // Determine the pictures to display
-   const displayedPics =
-   activeCategory === "All"
-     ? Gallery // Show all categories
-     : Gallery.filter((category) => category.title === activeCategory); // Filter based on category
- 
+  // Determine the pictures to display
+  const displayedPics =
+    activeCategory === "All"
+      ? Gallery // Show all categories
+      : Gallery.filter((category) => category.title === activeCategory); // Filter based on category
+
   return (
     <div className="">
       <div className="navbar w-full bg-purple-500 flex justify-between px-[5%]">
@@ -352,29 +406,31 @@ export const Home = () => {
         </div>
       </div>
       <div className="relative w-full">
-  {/* Background with Overlay */}
-  <div
-    className="bg-pink-400  bg-opacity-100 absolute top-0 left-0 w-full h-full"
-    style={{ backgroundImage: `url('./hawai.jpg')`, backgroundSize: 'cover', backgroundPosition: 'center', opacity: 1  }}
-  >
+        {/* Background with Overlay */}
+        <div
+          className="absolute w-full h-full"
+          style={{
+            backgroundImage: `url('./hawai.jpg')`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
 
-  </div>
-
-  {/* Content */}
-  <div className="relative text-white p-8 flex flex-col space-y-8 bg-pink-700 bg-opacity-90">
-    {stats.map((item, index) => (
-      <div key={index} className="flex items-center space-x-4">
-        <div className="text-4xl">{item.icon}</div>
-        <div>
-          <p className="text-4xl font-bold">{item.number}</p>
-          <p className="uppercase font-medium">{item.label}</p>
+        {/* Content */}
+        <div className="relative text-white p-8 flex flex-col space-y-8 bg-pink-700 bg-opacity-90">
+          {stats.map((item, index) => (
+            <div key={index} className="flex items-center space-x-4">
+              <div className="text-4xl">{item.icon}</div>
+              <div>
+                <p className="text-4xl font-bold">{item.number}</p>
+                <p className="uppercase font-medium">{item.label}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    ))}
-  </div>
-</div>
-    <div className="w-full flex justify-center flex-col items-center text-center space-y-5 p-[5%]">
-    <h1 className="z-10 text-[23px]">
+      <div className="w-full flex justify-center flex-col items-center text-center space-y-5 p-[5%]">
+        <h1 className="z-10 text-[23px]">
           <span className="text-pink-500">Our </span>Experts
         </h1>
         <div className="flex items-center gap-4">
@@ -390,35 +446,38 @@ export const Home = () => {
           taciti sociosqu ad litora torquent per conubia nostra, per inceptos
           himenaeos.
         </p>
-    {experts.map((item, index) => (
-        <div key={index} className="gap-5 flex flex-col">
-        <img src={item.image} alt="Camila" className="w-full " />
-        <div className="flex justify-center flex-col gap-5">
-          <h2 className="text-[24px] text-center text-xl font-semibold">{item.name}</h2>
-          <p className="text-[16px] text-gray-600 text-sm">{item.role}</p>
+        {experts.map((item, index) => (
+          <div key={index} className="gap-5 flex flex-col">
+            <img src={item.image} alt="Camila" className="w-full " />
+            <div className="flex justify-center flex-col gap-5">
+              <h2 className="text-[24px] text-center text-xl font-semibold">
+                {item.name}
+              </h2>
+              <p className="text-[16px] text-gray-600 text-sm">{item.role}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex items-center justify-center w-full py-[65px] px-[40px] bg-pink-500">
+        <div className="bg-pink-400 rounded-[50px] p-8 max-w-sm shadow-lg text-center">
+          <h2 className="text-white text-[32px] font-bold mb-2">
+            Our Special Offer
+          </h2>
+          <p className="text-white text-[18px] font-semibold mb-4">
+            YOU OWE YOURSELF THIS MOMENT
+          </p>
+          <p className="text-white text-[14px] mb-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+            hendrerit elit turpis, a porttitor tellus sollicitudin squ ad litora
+            torquent per conubia.
+          </p>
+          <button className="bg-pink-600 text-white py-2 px-6 rounded-md hover:bg-pink-700">
+            VIEW PACKAGES
+          </button>
         </div>
       </div>
-    ))}
-
-
-
-    </div>
-    <div className="flex items-center justify-center w-full py-[65px] px-[40px] bg-pink-500">
-      <div className="bg-pink-400 rounded-[50px] p-8 max-w-sm shadow-lg text-center">
-        <h2 className="text-white text-[32px] font-bold mb-2">Our Special Offer</h2>
-        <p className="text-white text-[18px] font-semibold mb-4">YOU OWE YOURSELF THIS MOMENT</p>
-        <p className="text-white text-[14px] mb-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-          hendrerit elit turpis, a porttitor tellus sollicitudin squ ad litora
-          torquent per conubia.
-        </p>
-        <button className="bg-pink-600 text-white py-2 px-6 rounded-md hover:bg-pink-700">
-          VIEW PACKAGES
-        </button>
-      </div>
-    </div>
-    <div className="w-full flex justify-center flex-col items-center text-center space-y-5 p-[5%]">
-    <h1 className="z-10 text-[23px]">
+      <div className="w-full flex justify-center flex-col items-center text-center space-y-5 p-[5%]">
+        <h1 className="z-10 text-[23px]">
           <span className="text-pink-500">Our </span>Gallery
         </h1>
         <div className="flex items-center gap-4">
@@ -469,49 +528,175 @@ export const Home = () => {
           </div>
         ))}
       </div> */}
-      <div className="p-4">
-      {/* Category Buttons */}
-      <div className="flex flex-wrap  text-sm font-medium mb-4">
-        <button
-          className={`py-2 px-4 rounded ${
-            activeCategory === "All"
-              ? " text-pink-500"
-              : " text-gray-800"
-          }`}
-          onClick={() => setActiveCategory("All")}
-        >
-          All
-        </button>
-        {Gallery.map((gallery) => (
-          <button
-            key={gallery.title}
-            className={`py-2 px-4 rounded cursor-pointer ${
-              activeCategory === gallery.title
-                ? "text-pink-500 "
-                : " text-gray-800 hover:bg-gray-300"
-            }`}
-            onClick={() => setActiveCategory(gallery.title)}
-          >
-            {gallery.title}
-          </button>
-        ))}
-      </div>
+        <div className="p-4">
+          {/* Category Buttons */}
+          <div className="flex flex-wrap  text-sm font-medium mb-4">
+            <button
+              className={`py-2 px-4 rounded ${
+                activeCategory === "All" ? " text-pink-500" : " text-gray-800"
+              }`}
+              onClick={() => setActiveCategory("All")}
+            >
+              All
+            </button>
+            {Gallery.map((gallery) => (
+              <button
+                key={gallery.title}
+                className={`py-2 px-4 rounded cursor-pointer ${
+                  activeCategory === gallery.title
+                    ? "text-pink-500 "
+                    : " text-gray-800 hover:bg-gray-300"
+                }`}
+                onClick={() => setActiveCategory(gallery.title)}
+              >
+                {gallery.title}
+              </button>
+            ))}
+          </div>
 
-      {/* Gallery Grid */}
-      <div className="">
-        {displayedPics.map((category, index) => (
-          <div key={index} className="bg-gray-100 rounded shadow">
-            <img
-              src={category.image}
-              alt={category.title}
-              className="w-full h-40 object-cover rounded"
-            />
-            
+          {/* Gallery Grid */}
+          <div className="">
+            {displayedPics.map((category, index) => (
+              <div key={index} className="bg-gray-100 rounded shadow">
+                <img
+                  src={category.image}
+                  alt={category.title}
+                  className="w-full h-40 object-cover rounded"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex justify-center flex-col items-center text-center space-y-5 p-[5%]">
+        <h1 className="z-10 text-[23px]">
+          <span className="text-pink-500">Our </span>Price
+        </h1>
+        <div className="flex items-center gap-4">
+          <div className="w-[70px] bg-[#ec4899] h-[2px]"></div>
+          <div>
+            <FaLeaf />
+          </div>
+          <div className="w-[70px] bg-[#ec4899] h-[2px]"></div>
+        </div>
+        <p className="text-[16px] text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+          hendrerit elit turpis, a porttitor tellus sollicitudin at.Class aptent
+          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos.
+        </p>
+
+        {priceplan.map((priceplan, index) => (
+          <div
+            key={index}
+            className="w-full bg-white rounded-lg shadow-lg overflow-hidden  border-b-4 border-pink-500"
+          >
+            {/* Image Section */}
+            <div
+              className="w-full relative h-48"
+              style={{
+                backgroundImage: `url('${priceplan.image}')`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div className="w-full flex flex-col justify-end gap-1 py-5  h-full bg-black bg-opacity-70 ">
+                <h3 className=" font-normal text-[20px] text-white">
+                  {priceplan.title}
+                </h3>
+                <p className="text-[44px] font-semibold text-white  ">
+                  ${priceplan.price}
+                  <span className="text-lg font-medium">/M</span>
+                </p>
+              </div>
+            </div>
+
+            {/* Plan Details */}
+            <div className="p-6 text-center ">
+              {/* Features */}
+              <ul className="mt-4 flex flex-col text-gray-600 space-y-2">
+                {priceplan.services.map((service, idx) => (
+                  <li key={idx} className="flex items-center justify-center">
+                    {service}
+                  </li>
+                ))}
+              </ul>
+
+              {/* Purchase Button */}
+              <button className="mt-6 bg-pink-500 text-white px-6 py-2 rounded-full hover:bg-pink-600 transition duration-300">
+                PURCHASE
+              </button>
+            </div>
           </div>
         ))}
       </div>
-    </div>
-    </div>
+      <div className="w-full flex justify-center flex-col items-center text-center space-y-5 p-[5%]">
+        <h1 className="z-10 text-[23px]">
+          <span className="text-pink-500">Our </span>Product
+        </h1>
+        <div className="flex items-center gap-4">
+          <div className="w-[70px] bg-[#ec4899] h-[2px]"></div>
+          <div>
+            <FaLeaf />
+          </div>
+          <div className="w-[70px] bg-[#ec4899] h-[2px]"></div>
+        </div>
+        <p className="text-[16px] text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+          hendrerit elit turpis, a porttitor tellus sollicitudin at.Class aptent
+          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos.
+        </p>
+
+        {productCat.map((el, index) => (
+          <div
+            key={index}
+            className="w-full bg-white rounded-lg  overflow-hidden  border-gray-200"
+          >
+            {/* Product Image */}
+            <div className="w-full h-full bg-gray-100">
+              <img
+                src={el.img} // Replace with the actual product image URL
+                alt="Massage Soap"
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            {/* Product Details */}
+            <div className="p-4 text-center">
+              <h3 className="text-lg font-semibold text-gray-800">
+                {el.title}
+              </h3>
+              <p className="text-pink-500 text-xl font-bold mt-2">
+                ${el.price}
+              </p>
+
+              {/* Add to Cart Button */}
+              <button className="mt-4  bg-pink-500 text-white p-2 rounded hover:bg-pink-600 transition duration-300">
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="w-full flex justify-center flex-col items-center text-center space-y-5 p-[5%] bg-gray-200">
+        <h1 className="z-10 text-[23px]">
+          <span className="text-pink-500">What </span>People Say
+        </h1>
+        <div className="flex items-center gap-4">
+          <div className="w-[70px] bg-[#ec4899] h-[2px]"></div>
+          <div>
+            <FaLeaf />
+          </div>
+          <div className="w-[70px] bg-[#ec4899] h-[2px]"></div>
+        </div>
+        <p className="text-[16px] text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+          hendrerit elit turpis, a porttitor tellus sollicitudin at.Class aptent
+          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos.
+        </p>
+      </div>
     </div>
   );
 };
